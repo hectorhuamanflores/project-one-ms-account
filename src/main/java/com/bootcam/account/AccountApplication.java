bc6dc0e14abb5a8234dadaed030a68f40e8eb315
Hector Huaman Flores <hhuamanf@emeal.nttdata.com>
2022-05-24 00:15:48 -0500

    Se agrego todo lo solicitado en proyecto 02

--- src/main/java/com/bootcam/account/AccountApplication.java
+++ src/main/java/com/bootcam/account/AccountApplication.java
@@ -2,12 +2,19 @@ package com.bootcam.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AccountApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
 	}
-
+    
+	
+	
 }
